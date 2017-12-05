780b4cdc22f5f9e4ce949cbeb01903a0b59a7ecc
Ashris Choudhury <ashris.me@gmail.com>
2017-12-05 00:43:10 -0500

    g

--- processingcode.js
+++ processingcode.js
@@ -10,19 +10,23 @@ function setup() {
 }
 
 function draw() {
-  background(255);
+  background(30);
   translate(width/2,height/2);
 
 
   for(var i=0;i<10;i++){
   	rotate(radians(angledata["A"+i]));
   	  strokeWeight(3);
-  stroke(0);
-  	line(0,0,0,100);
-  	fill(255,0,0);
+  stroke(255,0,0);
+  	line(0,0,0,150);
+  	fill(255);
   	noStroke();
-  	text("A"+i,0,100);
-  	translate(0,100);
+  	if(i>0){
+  		ellipse(0,0,10,10);
+  		text("A"+i+" ("+parseInt(angledata["A"+i])+")",10,10);
+  	}
+
+  	translate(0,150);
   }
 
 
@@ -39,4 +43,10 @@ function Angles() {
   this.A7=Math.random()*angmax;
   this.A8=Math.random()*angmax;
   this.A9=Math.random()*angmax;
+}
+function isRight(n){
+	if(around(90,n) || around(270,n))true;
+}
+function around(Q,n){
+	if(abs(Q-n)<10)return true;
 }
